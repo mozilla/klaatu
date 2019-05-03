56452d6c89aae7098652c30996fecaf967827b27
Benjamin Forehand Jr <bennyjr169@gmail.com>
2019-05-03 17:36:40 -0400

    Fix lint error.

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,23 +14,21 @@ def pytest_addoption(parser):
 
 @pytest.fixture
 def firefox_options(firefox_options):
-    firefox_options.set_preference(
-        'extensions.install.requireBuiltInCerts', False)
-    firefox_options.set_preference('ui.popup.disable_autohide', True)
-    firefox_options.set_preference('xpinstall.signatures.required', False)
-    firefox_options.set_preference('extensions.webapi.testing', True)
-    firefox_options.set_preference('extensions.legacy.enabled', True)
-    firefox_options.set_preference('browser.tabs.remote.autostart', True)
-    firefox_options.set_preference('browser.tabs.remote.autostart.1', True)
-    firefox_options.set_preference('browser.tabs.remote.autostart.2', True)
-    firefox_options.set_preference('devtools.chrome.enabled', True)
-    firefox_options.set_preference('devtools.debugger.remote-enabled', True)
-    firefox_options.set_preference('devtools.debugger.prompt-connection', False)
-    firefox_options.set_preference('shieldStudy.logLevel', 'All')
-    firefox_options.add_argument('-foreground')
-    firefox_options.log.level = 'trace'
+    firefox_options.set_preference("extensions.install.requireBuiltInCerts", False)
+    firefox_options.set_preference("ui.popup.disable_autohide", True)
+    firefox_options.set_preference("xpinstall.signatures.required", False)
+    firefox_options.set_preference("extensions.webapi.testing", True)
+    firefox_options.set_preference("extensions.legacy.enabled", True)
+    firefox_options.set_preference("browser.tabs.remote.autostart", True)
+    firefox_options.set_preference("browser.tabs.remote.autostart.1", True)
+    firefox_options.set_preference("browser.tabs.remote.autostart.2", True)
+    firefox_options.set_preference("devtools.chrome.enabled", True)
+    firefox_options.set_preference("devtools.debugger.remote-enabled", True)
+    firefox_options.set_preference("devtools.debugger.prompt-connection", False)
+    firefox_options.set_preference("shieldStudy.logLevel", "All")
+    firefox_options.add_argument("-foreground")
+    firefox_options.log.level = "trace"
     return firefox_options
-
 
 
 @pytest.fixture
