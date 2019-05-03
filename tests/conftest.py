93465e9b982fcac07c2092a146583aaecb6229f0
Benjamin Forehand Jr <bennyjr169@gmail.com>
2019-05-03 17:53:58 -0400

    Actually install the addon. (#4)
    * Actually install the addon.
    
    * Remove temporary flag.
    
    * Add firefox options setup.
    
    * Fix lint error.
    
    * Fix artifacts.
    
    * Fix setup config file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 
@@ -10,8 +12,28 @@ def pytest_addoption(parser):
     )
 
 
-def selenium(config, selenium):
+@pytest.fixture
+def firefox_options(firefox_options):
+    firefox_options.set_preference("extensions.install.requireBuiltInCerts", False)
+    firefox_options.set_preference("ui.popup.disable_autohide", True)
+    firefox_options.set_preference("xpinstall.signatures.required", False)
+    firefox_options.set_preference("extensions.webapi.testing", True)
+    firefox_options.set_preference("extensions.legacy.enabled", True)
+    firefox_options.set_preference("browser.tabs.remote.autostart", True)
+    firefox_options.set_preference("browser.tabs.remote.autostart.1", True)
+    firefox_options.set_preference("browser.tabs.remote.autostart.2", True)
+    firefox_options.set_preference("devtools.chrome.enabled", True)
+    firefox_options.set_preference("devtools.debugger.remote-enabled", True)
+    firefox_options.set_preference("devtools.debugger.prompt-connection", False)
+    firefox_options.set_preference("shieldStudy.logLevel", "All")
+    firefox_options.add_argument("-foreground")
+    firefox_options.log.level = "trace"
+    return firefox_options
+
+
+@pytest.fixture
+def selenium(pytestconfig, selenium):
     """Setup Selenium"""
-    addon = config.getoption("--experiment")
-    selenium.install_addon(addon)
+    addon = pytestconfig.getoption("--experiment")
+    selenium.install_addon(os.path.abspath(addon))
     return selenium
