aaae8cc1d37a5711a60200746d0eedbeda527ae7
Benjamin Forehand Jr <bennyjr169@gmail.com>
2019-05-09 17:26:45 -0400

    Add private browsing test. (#7)
    * Add private browsing test.
    
    * Fix typo.
    
    * Fix lint.
    
    * Fix return value.

--- tests/test_with_browser.py
+++ tests/test_with_browser.py
@@ -1,7 +1,22 @@
 import pytest
 
+from tests.toolbar import ToolBar
+
 
 @pytest.mark.nondestructive
-def test_experiment_does_not_stop_startup(firefox, selenium):
+def test_experiment_does_not_stop_startup(selenium):
     """Experiment does not stop browser startup, or prohibit a clean exit."""
     selenium.get("https://www.mozilla.org")
+
+
+@pytest.mark.nondestructive
+def test_private_browsing_disables_experiment(firefox, selenium):
+    """Experiment should be disabled in private browsing mode."""
+    new_browser = firefox.browser.open_window(private=True)
+    assert new_browser.is_private
+    toolbar = ToolBar(selenium)
+    for item in toolbar.toolbar_items:
+        if "shield.mozilla.org" not in item._id:
+            continue
+        else:
+            raise AssertionError("Extension is Found")
