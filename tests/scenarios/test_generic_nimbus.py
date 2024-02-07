4f815ea2b90f357ca4d0b7a7354077ed28bcb7c0
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-02-07 14:28:57 -0600

    Add exp-4327 as well as switch to poetry.

---
+++ tests/scenarios/test_generic_nimbus.py
@@ -0,0 +1,31 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+import requests
+from pytest_bdd import scenario, given, then
+
+from tests.expected import (
+    firefox_update_banner_is_found,
+    firefox_update_banner_is_invisible,
+)
+
+
+@scenario(
+    "../features/generic_nimbus.feature",
+    "The browser will enroll into the requested branch",
+)
+def test_experiment_enrolls_into_correct_branch(): ...
+
+
+@given("Firefox is launched enrolled in an Experiment")
+def selenium(selenium):
+    return selenium
+
+
+@then("The experiment branch should be correctly reported")
+def check_branch_in_telemetry(check_ping_for_experiment, request, variables):
+    experiment_branch = request.config.getoption("--experiment-branch")
+    data = check_ping_for_experiment(f"optin-{variables['slug']}")
+    assert experiment_branch in data["branch"]
