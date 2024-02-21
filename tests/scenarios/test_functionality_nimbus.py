e340f12b4ab0f7a0fb7720d55a244a5f2573e13a
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-02-21 17:42:45 -0600

    Add exp-4336: Check opening a new tab functions correctly.

--- tests/scenarios/test_functionality_nimbus.py
+++ tests/scenarios/test_functionality_nimbus.py
@@ -21,10 +21,14 @@ def navigate_using_url_bar(selenium, cmd_or_ctrl_button):
                 ActionChains(selenium).move_to_element(el).pause(1).click().pause(1).key_down(
                     cmd_or_ctrl_button
                 ).send_keys("v").key_up(cmd_or_ctrl_button).send_keys(Keys.ENTER).perform()
+                return
             else:
                 el.click()
                 el.send_keys(text)
                 el.send_keys(Keys.ENTER)
+        WebDriverWait(selenium, 60).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".loaded"))
+        )
 
     return _navigate_function
 
@@ -45,9 +49,23 @@ def test_browser_accepts_a_string():
     pass
 
 
+@scenario(
+    "../features/generic_functionality.feature",
+    "The browser will allow a new tab to be opened",
+)
+def test_browser_loads_a_new_tab():
+    pass
+
+
 @given("Firefox is launched enrolled in an Experiment")
 def selenium(selenium):
     return selenium
+
+
+@given("Firefox has loaded a webpage")
+def load_mozilla_wepage(navigate_using_url_bar, selenium):
+    navigate_using_url_bar()
+    assert "allizom" in selenium.current_url
 
 
 @then("Firefox should still accept a URL into the search bar")
@@ -79,3 +97,17 @@ def copy_and_paste_string_to_url_bar(cmd_or_ctrl_button, selenium, navigate_usin
     navigate_using_url_bar(use_clipboard=True)
 
     WebDriverWait(selenium, 60).until(EC.title_contains(el.text))
+
+
+@then("Firefox should be allowed to open a new tab")
+def open_a_new_tab(selenium):
+    with selenium.context(selenium.CONTEXT_CHROME):
+        el = selenium.find_element(By.CSS_SELECTOR, "#tabs-newtab-button")
+        el.click()
+
+
+@then("The tab should open successfully")
+def check_new_tab(selenium):
+    # get the last tab
+    selenium.switch_to.window(selenium.window_handles[-1])
+    assert "about:newtab" in selenium.current_url
