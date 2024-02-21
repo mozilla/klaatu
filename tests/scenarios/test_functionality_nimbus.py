ae95a63a1ed1f98a84025b2114442d13e5bcfce1
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-02-21 16:22:55 -0600

    Fix title errors.

--- tests/scenarios/test_functionality_nimbus.py
+++ tests/scenarios/test_functionality_nimbus.py
@@ -1,7 +1,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-import sys
 
 import pytest
 from pytest_bdd import given, scenario, then
@@ -79,7 +78,4 @@ def copy_and_paste_string_to_url_bar(cmd_or_ctrl_button, selenium, navigate_usin
     ).send_keys("c").key_up(cmd_or_ctrl_button).perform()
     navigate_using_url_bar(use_clipboard=True)
 
-    copied_text = el.text
     WebDriverWait(selenium, 60).until(EC.title_contains(el.text))
-    search = selenium.find_element(By.CSS_SELECTOR, ".title")
-    assert copied_text == search.text
