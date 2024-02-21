00e76d94b546146d6d4b9618d0491b1b9993dae5
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-02-21 14:19:33 -0600

    Add exp-4335: Copy/Paste text test.

--- tests/scenarios/test_functionality_nimbus.py
+++ tests/scenarios/test_functionality_nimbus.py
@@ -1,12 +1,35 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import sys
 
+import pytest
 from pytest_bdd import given, scenario, then
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
+
+@pytest.fixture
+def navigate_using_url_bar(selenium):
+    def _navigate_function(text=None, use_clipboard=False):
+        cmd_ctrl = Keys.COMMAND if sys.platform == "darwin" else Keys.CONTROL
+
+        if not text:
+            text = "https://www.allizom.org/en-US/"
+        with selenium.context(selenium.CONTEXT_CHROME):
+            el = selenium.find_element(By.CSS_SELECTOR, "#urlbar-input")
+            if use_clipboard:
+                ActionChains(selenium).move_to_element(el).pause(1).click().pause(1).key_down(
+                    cmd_ctrl
+                ).send_keys("v").key_up(cmd_ctrl).send_keys(Keys.ENTER).perform()
+            else:
+                el.click()
+                el.send_keys(text)
+                el.send_keys(Keys.ENTER)
+
+    return _navigate_function
 
 
 @scenario(
@@ -17,18 +40,22 @@ def test_browser_navigates_to_url_correctly():
     pass
 
 
+@scenario(
+    "../features/generic_functionality.feature",
+    "The browser's URL bar will navigate to the supplied string",
+)
+def test_browser_accepts_a_string():
+    pass
+
+
 @given("Firefox is launched enrolled in an Experiment")
 def selenium(selenium):
     return selenium
 
 
 @then("Firefox should still accept a URL into the search bar")
-def navigate_to_url(selenium):
-    with selenium.context(selenium.CONTEXT_CHROME):
-        el = selenium.find_element(By.CSS_SELECTOR, "#urlbar-input")
-        el.click()
-        el.send_keys("https://www.allizom.org/en-US/")
-        el.send_keys(Keys.ENTER)
+def navigate_to_url(navigate_using_url_bar):
+    navigate_using_url_bar()
 
 
 @then("The URL should load the webpage successfully")
@@ -37,3 +64,32 @@ def check_url_page_loads_correctly(selenium):
         EC.visibility_of_element_located((By.CSS_SELECTOR, ".c-navigation-logo-image")),
         message="Navigation logo not found",
     )
+
+
+@then("Firefox should still accept a copied string that is sent to the search bar")
+def copy_and_paste_string_to_url_bar(selenium, navigate_using_url_bar):
+    cmd_ctrl = Keys.COMMAND if sys.platform == "darwin" else Keys.CONTROL
+
+    selenium.get("http://www.allizom.org/en-US/")
+    el = selenium.find_element(
+        By.CSS_SELECTOR, "section.mzp-c-footer-section:nth-child(1) > h5:nth-child(1)"
+    )
+
+    # scroll down to text
+    selenium.execute_script("arguments[0].scrollIntoView(true);", el)
+
+    ActionChains(selenium).move_to_element(el).pause(1).double_click(el).key_down(
+        cmd_ctrl
+    ).send_keys("c").key_up(cmd_ctrl).perform()
+    navigate_using_url_bar(use_clipboard=True)
+
+    copied_text = el.text
+    WebDriverWait(selenium, 60).until(
+        EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "body div#searchform div form div div div div div textarea")
+        )
+    )
+    search = selenium.find_element(
+        By.CSS_SELECTOR, "body div#searchform div form div div div div div textarea"
+    )
+    assert copied_text == search.text
