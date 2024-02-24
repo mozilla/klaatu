4ef4ff721ebf031750748048ec1ded93ab13f6d6
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-02-23 18:42:33 -0600

    Fix exp-4338: Add langage pack install test.

--- tests/scenarios/test_functionality_nimbus.py
+++ tests/scenarios/test_functionality_nimbus.py
@@ -1,6 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import time
 
 import pytest
 from pytest_bdd import given, scenario, then
@@ -65,8 +66,17 @@ def test_browser_loads_a_new_tab_via_keyboard():
     pass
 
 
+@scenario(
+    "../features/generic_functionality.feature",
+    "The browser will allow language packs to be installed",
+)
+def test_browser_allows_a_language_pack_to_be_installed():
+    pass
+
+
 @given("Firefox is launched enrolled in an Experiment")
 def selenium(selenium):
+    selenium.implicitly_wait(5)
     return selenium
 
 
@@ -126,3 +136,71 @@ def check_new_tab(selenium):
     # get the last tab
     selenium.switch_to.window(selenium.window_handles[-1])
     assert "about:newtab" in selenium.current_url
+
+
+@then("The user will install a language pack")
+def install_acholi_language_pack(selenium, request):
+
+    add_button_locator = (By.CSS_SELECTOR, "#add")
+    browser_dialog_box_locator = (By.CSS_SELECTOR, "#BrowserLanguagesDialog")
+    root_dialog_box_locator = (
+        By.CSS_SELECTOR,
+        "vbox.dialogOverlay:nth-child(1) > vbox:nth-child(1) > browser:nth-child(2)",
+    )
+    language_button_locator = (By.CSS_SELECTOR, "#manageBrowserLanguagesButton")
+    language_search_locator = (By.CSS_SELECTOR, ".in-menulist menuitem label")
+    menu_list_locator = (By.CSS_SELECTOR, ".languages-grid #availableLocales .in-menulist")
+
+    if not request.config.getoption("--run-firefox-release"):
+        pytest.skip("needs --run-firefox-release option to run")
+        return
+
+    selenium.get("about:preferences")
+    button = selenium.find_element(*language_button_locator)
+    button.click()
+    WebDriverWait(selenium, 60).until(EC.visibility_of_element_located(root_dialog_box_locator))
+    dialog = selenium.find_element(*root_dialog_box_locator)
+    selenium.switch_to.frame(dialog)
+
+    dialog = selenium.find_element(*browser_dialog_box_locator)
+    menu_list = selenium.find_element(*menu_list_locator)
+    menu_list.click()
+    WebDriverWait(menu_list, 60).until(EC.visibility_of_element_located(language_search_locator))
+    el = menu_list.find_element(*language_search_locator)
+    ActionChains(selenium).move_to_element(el).pause(1).click().perform()
+    menu_list.click()
+    list = menu_list.find_elements(By.CSS_SELECTOR, "menuitem")
+    for item in list:
+        if "Acholi" in item.get_attribute("label"):
+            ActionChains(selenium).move_to_element(item).pause(1).click().pause(1).perform()
+            break
+    WebDriverWait(dialog, 60).until(
+        EC.element_to_be_clickable(add_button_locator), message="Language was not added"
+    )
+    dialog.find_element(*add_button_locator).click()
+
+    time.sleep(15)  # need to use a hard wait for the langage pack to download
+
+
+@then("Firefox will be correctly localized for the installed language pack")
+def check_for_localized_firefox(selenium):
+    locales_locator = (By.CSS_SELECTOR, "#primaryBrowserLocale")
+    text_locator = (By.CSS_SELECTOR, "#browserLanguagesBox > description:nth-child(1)")
+
+    selenium.get("about:preferences")
+    locales = selenium.find_element(*locales_locator)
+    locales.click()
+    list = WebDriverWait(locales, 60).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "menuitem"))
+    )
+    for item in list:
+        if "Acholi" in item.get_attribute("label"):
+            ActionChains(selenium).move_to_element(item).pause(1).click().pause(1).perform()
+            break
+    WebDriverWait(selenium, 60).until(
+        EC.text_to_be_present_in_element(
+            text_locator,
+            "Yer leb ma kitiyo kwedgi me nyuto jami ayera, kwena, ki jami angeya ki ii Firefox.",
+        ),
+        message="Language switch didn't happen",
+    )
