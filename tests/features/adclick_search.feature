76996519ba6c7a9ff8b17c4184925212275a9291
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-03-01 13:03:48 -0600

    Exp 4340 3 (#82)
    * Work on EXP-4340: Add context click search telemetry test.
    
    * Fix linting.

--- tests/features/adclick_search.feature
+++ tests/features/adclick_search.feature
@@ -14,3 +14,10 @@ Feature: Tests that click ads and verify telemetry
         And The user searches for something in the search bar that will return ads
         Then The user clicks on an ad
         And The browser reports correct telemetry for the searchbar adclick event
+
+    @smoke
+    Scenario: Telemetry reports correctly for context menu adclick search events
+        Given Firefox is launched enrolled in an Experiment
+        And The user highlights some text and wants to search for it via the context menu
+        Then The user clicks on an ad
+        And The browser reports correct telemetry for the contextmenu adclick event
---
+++ tests/scenarios/test_ads_search_telemetry.py
@@ -0,0 +1,119 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import time
+
+from pytest_bdd import given, parsers, scenarios, then
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+scenarios("../features/withads_search.feature", "../features/adclick_search.feature")
+
+
+@given("The user searches for something in the search bar that will return ads")
+def search_using_search_bar_to_return_ads(selenium):
+    pref_list_locator = (By.CSS_SELECTOR, "#prefs tr td span")
+
+    # enable search bar using pref menu since it seems blocked via geckodriver
+    selenium.get("about:config")
+    pref_search_box = selenium.find_element(By.CSS_SELECTOR, "#about-config-search")
+    pref_search_box.click()
+    pref_search_box.send_keys("browser.search.widget.inNavBar")
+    WebDriverWait(selenium, 60).until(EC.visibility_of_element_located(pref_list_locator))
+    pref_list = selenium.find_elements(*pref_list_locator)
+    for item in pref_list:
+        if item.get_attribute("data-l10n-id") == "about-config-pref-accessible-value-default":
+            ActionChains(selenium).move_to_element(item).pause(1).double_click().perform()
+            break
+
+    # perform search
+    with selenium.context(selenium.CONTEXT_CHROME):
+        search_bar = selenium.find_element(By.CSS_SELECTOR, "#search-container .searchbar-textbox")
+        search_bar.send_keys("buy stocks")
+        search_bar.send_keys(Keys.ENTER)
+
+
+@given("The user highlights some text and wants to search for it via the context menu")
+def search_using_context_click_menu(selenium, simplehttpserver):
+    selenium.get("http://localhost:8000")
+    el = selenium.find_element(By.CSS_SELECTOR, "#search-to-return-ads")
+
+    ActionChains(selenium).move_to_element(el).pause(1).double_click(el).pause(1).context_click(
+        el
+    ).perform()
+    with selenium.context(selenium.CONTEXT_CHROME):
+        menu = selenium.find_element(By.CSS_SELECTOR, "#contentAreaContextMenu")
+        menu.find_element(By.CSS_SELECTOR, "#context-searchselect").click()
+    WebDriverWait(selenium, 60).until(EC.number_of_windows_to_be(3))
+    selenium.switch_to.window(selenium.window_handles[1])
+    time.sleep(5)
+
+
+@then(parsers.parse("The browser reports correct telemetry for the {search:w} search event"))
+def check_telemetry_for_with_ads_search(find_ads_search_telemetry, search):
+    assert find_ads_search_telemetry(
+        f"browser.search.withads.{search}", ping_data={"google:tagged": 1}
+    )
+
+
+@then(parsers.parse("The browser reports correct telemetry for the {search:w} adclick event"))
+def check_telemetry_for_ad_click_search(find_ads_search_telemetry, search):
+    assert find_ads_search_telemetry(
+        f"browser.search.adclicks.{search}", ping_data={"google:tagged": 1}
+    )
+
+
+@given("The user searches for something that is likely to return ads")
+def search_using_url_bar_to_return_ads(navigate_using_url_bar):
+    navigate_using_url_bar(text="buy stocks")
+
+
+@then("The user clicks on an ad")
+def click_on_an_ad(selenium):
+    current_url = selenium.current_url
+    ads = selenium.find_elements(By.CSS_SELECTOR, "#tads a")
+    ads[0].click()
+    WebDriverWait(selenium, 5).until(EC.url_changes(current_url))
+
+
+@then("The user should be allowed to search on the new tab")
+def search_on_new_tab(selenium):
+    search_box = selenium.find_element(By.CSS_SELECTOR, ".search-handoff-button")
+    search_box.click()
+    with selenium.context(selenium.CONTEXT_CHROME):
+        el = selenium.find_element(By.CSS_SELECTOR, "#urlbar-input")
+        el.send_keys("Apple iphone")
+        el.send_keys(Keys.ENTER)
+
+
+@then("The user should be allowed to perform a background search in the new tab")
+def perform_background_search(selenium):
+    with selenium.context(selenium.CONTEXT_CHROME):
+        el = selenium.find_element(By.CSS_SELECTOR, "#urlbar-input")
+        el.send_keys("Apple iphone")
+        ActionChains(selenium).key_down(Keys.ALT).key_down(Keys.SHIFT).key_down(
+            Keys.ENTER
+        ).perform()
+
+
+@then("The page is refreshed")
+def refresh_page_and_close_browser(selenium):
+    # Need to close the browser to get the main ping to send
+    selenium.refresh()
+    time.sleep(15)  # wait a little to not cause a race condition
+    selenium.quit()
+
+
+@then("The page loads")
+def wait_for_ad_click_page_to_load(selenium):
+    WebDriverWait(selenium, 60).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "body")))
+
+
+@then("The user goes back to the search page")
+def go_back_one_page(selenium):
+    selenium.back()
+    time.sleep(10)  # wait some time after going back so the event can register
+    selenium.quit()
