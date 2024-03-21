72309cce43c47d19e298cbcb7b93cbae0c236634
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-03-21 10:31:25 -0500

    Final updates.

--- setup_script.sh
+++ setup_script.sh
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# script to setup search server
 set -ex
 
 git clone https://github.com/mozilla/gecko-dev.git --depth 1
