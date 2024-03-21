101ef607b3b0c2cd69706aecf4e0bc0522cc2201
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-03-21 10:31:43 -0500

    Fixes EXP-4421 (#104)
    * Work on EXP-4421
    
    * Updates.
    
    * Change storage dir.
    
    * Final updates.

---
+++ setup-windows.ps1
@@ -0,0 +1,15 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# script to install firefox and dependencies
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco feature enable -n allowGlobalConfirmation
+
+choco install jdk8 selenium-gecko-driver selenium firefox python git 
+
+choco install firefox-nightly --pre
+
+choco install firefox-beta --pre
---
+++ setup_script.sh
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# script to setup search server
+set -ex
+
+git clone https://github.com/mozilla/gecko-dev.git --depth 1
+
+mkdir search_files
+
+cp gecko-dev/browser/components/search/test/browser/telemetry/* search_files/
+
+cp search_server/* search_files/
