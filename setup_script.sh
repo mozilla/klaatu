c80315bc7eb3d4cdacd5ec688a7857caee08ab35
Benjamin Forehand Jr <bennyjr169@gmail.com>
2024-03-21 10:08:05 -0500

    Updates.

--- setup_script.sh
+++ setup_script.sh
@@ -4,6 +4,6 @@ git clone https://github.com/mozilla/gecko-dev.git --depth 1
 
 mkdir search_files
 
-cp gecko-dev/browser/components/search/test/browser/telemetry search_files/
+cp gecko-dev/browser/components/search/test/browser/telemetry/* search_files/
 
 cp search_server/* search_files/
