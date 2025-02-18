a359068c7e051c30374965383b5958116468b7cd
Benjamin Forehand Jr <bennyjr169@gmail.com>
2025-02-18 12:28:14 -0600

    Add step to generate server certifications.

--- search_server/search_server.py
+++ search_server/search_server.py
@@ -10,7 +10,7 @@ import ssl
 httpd = HTTPServer(('localhost', 8888), SimpleHTTPRequestHandler)
 
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(os.getenv("SSL_CERT"), os.getenv("SSL_KEY"))
+context.load_cert_chain("server.cert", "server.key")
 
 # Set the SSL context for the server
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
