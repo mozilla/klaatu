b296d6e43d2af49a1319e42d4f0fb5850c3e1c03
Benjamin Forehand Jr <bennyjr169@gmail.com>
2025-02-18 12:12:18 -0600

    chore(project): Remove search server certifications from repo.

--- search_server/search_server.py
+++ search_server/search_server.py
@@ -3,13 +3,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
 import ssl
 
 
 httpd = HTTPServer(('localhost', 8888), SimpleHTTPRequestHandler)
 
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain("server.cert", "server.key")
+context.load_cert_chain(os.getenv("SSL_CERT"), os.getenv("SSL_KEY"))
 
 # Set the SSL context for the server
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
